2edb80803ed66e9303522628a38d43a778d19c6f
Alihaggag11 <ali7aggag@gmail.com>
2024-09-26 17:03:42 +0300

    toast fix

--- app/components/ui/toaster.tsx
+++ app/components/ui/toaster.tsx
@@ -29,7 +29,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport />
+      <ToastViewport className="fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]" />
     </ToastProvider>
   )
 }
